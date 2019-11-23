cc31d6db1aeff000a0e48f2d5a832988e7a3f939
Your Name <you@example.com>
2019-11-24 02:17:00 +0900

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-go run main.go --cloud-provider=cloud --use-service-account-credentials=true  --leader-elect=true #--client-ca-file=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+# go run main.go --cloud-provider=cloud --use-service-account-credentials=true  --leader-elect=true #--client-ca-file=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+
+
+
+kubectl taint nodes kind-control-plane node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule
+kubectl taint nodes kind-worker node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule
+kubectl taint nodes kind-worker2 node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule
+kubectl taint nodes kind-worker3 node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule
+
