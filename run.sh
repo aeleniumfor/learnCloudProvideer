77ea92fc518933d51b1a394332f01550080740eb
Your Name <you@example.com>
2019-11-24 11:23:20 +0900

    kind create cluster

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 # go run main.go --cloud-provider=cloud --use-service-account-credentials=true  --leader-elect=true #--client-ca-file=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
 
-        - --use-service-account-credentials
+        # - --use-service-account-credentials
+
+kind create cluster --config $(pwd)/kubernetes/multi.yml
+export KUBECONFIG="$(kind get kubeconfig-path)"
 
 kubectl taint nodes kind-control-plane node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule
 kubectl taint nodes kind-worker node.cloudprovider.kubernetes.io/uninitialized=true:NoSchedule
